a4306ed901a0a977f22055796b7ec7fa27047010
mdmah <mdmahmud3311@gmail.com>
2022-09-16 11:06:48 +0600

    lcm

--- 2. Factors and Multiples/LCM of 3 or 4.py
+++ 2. Factors and Multiples/LCM of 3 or 4.py
@@ -2,15 +2,28 @@ number_one = 4
 number_two = 6
 number_three = 12
 number_four = 21
+number_five = 32
 
 print(number_one)
-# print(2**2)
+print(2 ** 2)
 print(number_two)
-# print(5*2)
+print(3 * 2)
 print(number_three)
-# print(3* 2**2)
+print(3 * 2 ** 2)
 print(number_four)
-# print(3*5)
+print(3 * 7)
+print(number_five)
+print(2 ** 2 * 2 ** 2 * 2)
+print(f"LMC= {2 ** 2 * 3 * 2 ** 2 * 7}")
 
-print(f"LCM = {(2**2) * 5 * 3 * 5}")
-
+# print(number_one)
+# # print(2**2)
+# print(number_two)
+# # print(5*2)
+# print(number_three)
+# # print(3* 2**2)
+# print(number_four)
+# # print(3*5)
+#
+# print(f"LCM = {(2**2) * 5 * 3 * 5}")
+#
