7b0335c7418ef720e8b30ba77884d03c208aa934
mdmah <mdmahmud3311@gmail.com>
2022-09-16 11:20:16 +0600

    Hcf

---
+++ HCM.py
@@ -0,0 +1,15 @@
+number_one = 10
+number_two = 20
+number_three = 30
+number_four = 40
+
+print(number_one)
+print(2, 5)
+print(number_two)
+print(2, 4, 5, 10)
+print(number_three)
+print(2, 3, 5, 6, 10,15)
+print(number_four)
+print(2, 4, 5, 8, 10, 20,)
+
+print(f"HCF={}")
